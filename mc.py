13b651f133226630ae1c88cdd777f5871885cf04
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-08-03 11:31:26 +0530

    all working good

--- mc.py
+++ mc.py
@@ -18,12 +18,11 @@ class MongoDBConnection(ExperimentalBaseConnection[pymongo.MongoClient]):
             database = kwargs.pop("database")
         else:
             database = self._secrets["database"]
-        
+
         if "collection_name" in kwargs:
             collection_name = kwargs.pop("collection_name")
         else:
             collection_name = self._secrets["collection_name"]
-
 
         client = pymongo.MongoClient(connection_string, **kwargs)
 
@@ -33,19 +32,91 @@ class MongoDBConnection(ExperimentalBaseConnection[pymongo.MongoClient]):
         @cache_data(ttl=ttl)
         def _find_all_documents():
             return pd.DataFrame(list(self._instance.find()))
+
         return _find_all_documents()
+
+    def find(self, filter: dict = None, ttl: int = 1000, **kwargs) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _find(
+            filter: dict = None,
+        ):
+            query = filter or {}
+
+            # Extract any additional query options from **kwargs
+            projection = kwargs.get("projection")
+            sort = kwargs.get("sort")
+            limit = kwargs.get("limit")
+            skip = kwargs.get("skip")
+
+            # Perform the find operation with additional query options
+            cursor = self._instance.find(
+                query, projection=projection, sort=sort, limit=limit, skip=skip
+            )
+
+            return pd.DataFrame(list(cursor))
+
+        return _find(filter)
+
+    def find_one(self, filter: dict = None, ttl: int = 1000, **kwargs) -> pd.Series:
+        def _find_one(filter: dict = None):
+            query = filter or {}
+
+            # Perform the find_one operation with additional query options
+            result = self._instance.find_one(query, projection=kwargs.get("projection"))
+
+            return pd.Series(result) if result else pd.Series()
+
+        return _find_one(filter)
 
     def insert_document(self, document: dict):
         return self._instance.insert_one(document)
 
-    def query(
-        self, database_name: str, collection_name: str, query: dict, ttl: int = 3600
+    def insert_many_documents(self, documents: list):
+        return self._instance.insert_many(documents)
+
+    def update_document(self, query: dict, update: dict):
+        return self._instance.update_one(query, {"$set": update})
+
+    def update_documents(self, query: dict, update: dict):
+        return self._instance.update_many(query, {"$set": update})
+
+    def delete_document(self, query: dict):
+        return self._instance.delete_one(query)
+
+    def delete_documents(self, query: dict):
+        return self._instance.delete_many(query)
+
+    def count_documents(self, query: dict, ttl: int = 1000):
+        @cache_data(ttl=ttl)
+        def _count_documents(query: dict):
+            return self._instance.count_documents(query)
+
+        return _count_documents(query)
+
+    def distinct_values(self, field: str, query: dict = None, ttl: int = 1000):
+        @cache_data(ttl=ttl)
+        def _distinct_values(field: str, query: dict = None):
+            return self._instance.distinct(field, filter=query)
+
+        return _distinct_values(field, query)
+
+    def query(self, query: dict, ttl: int = 3600) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _query(query: dict) -> pd.DataFrame:
+            return pd.DataFrame(list(self._instance.find(query)))
+
+        return _query(query)
+
+    def paginate_documents(
+        self, page_number: int, items_per_page: int, ttl: int = 1000
     ) -> pd.DataFrame:
         @cache_data(ttl=ttl)
-        def _query(
-            database_name: str, collection_name: str, query: dict
-        ) -> pd.DataFrame:
-            collection = self.get_collection(database_name, collection_name)
-            return pd.DataFrame(list(collection.find(query)))
+        def _paginate_documents(page_number: int, items_per_page: int) -> pd.DataFrame:
+            # Calculate the number of documents to skip based on the page number and items per page
+            skip_count = (page_number - 1) * items_per_page
 
-        return _query(database_name, collection_name, query)
+            # Perform the query with pagination
+            documents = self._instance.find().skip(skip_count).limit(items_per_page)
+            return pd.DataFrame(list(documents))
+
+        return _paginate_documents(page_number, items_per_page)
